228159453a7582da7ae29df10cc9c6195073df8b
ji-one <ds020228@naver.com>
2023-08-06 23:46:35 +0900

    feat: 템플릿 메서드 패턴 - 적용1

---
+++ src/main/java/hello/springcoreadvanced/trace/template/AbstractTemplate.java
@@ -0,0 +1,30 @@
+package hello.springcoreadvanced.trace.template;
+
+import hello.springcoreadvanced.trace.TraceStatus;
+import hello.springcoreadvanced.trace.logtrace.LogTrace;
+
+public abstract class AbstractTemplate<T> {
+    private final LogTrace trace;
+
+    protected AbstractTemplate(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    public T execute(String message) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+
+            // 로직 호출
+            T result = call();
+
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    protected abstract T call();
+}
