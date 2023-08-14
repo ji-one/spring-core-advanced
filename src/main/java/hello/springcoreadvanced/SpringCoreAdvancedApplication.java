6f6f9365ea81dc97d75895e0ef49df939163bf13
ji-one <ds020228@naver.com>
2023-08-15 00:45:19 +0900

    feat: 예제 프로젝트 만들기 v2

--- src/main/java/hello/springcoreadvanced/SpringCoreAdvancedApplication.java
+++ src/main/java/hello/springcoreadvanced/SpringCoreAdvancedApplication.java
@@ -1,11 +1,13 @@
 package hello.springcoreadvanced;
 
 import hello.springcoreadvanced.proxy.config.AppV1Config;
+import hello.springcoreadvanced.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+//@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.springcoreadvanced.proxy.app")
 public class SpringCoreAdvancedApplication {
 
--- src/main/java/hello/springcoreadvanced/proxy/app/v1/OrderControllerV1.java
+++ src/main/java/hello/springcoreadvanced/proxy/app/v1/OrderControllerV1.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping // 스프링은 @Controller 또는 @RequestMapping 이 있어야 스프링 컨트롤러로 인식
+@RequestMapping // 스프링은 @Controller 또는 @RequestMapping 이 있어야 스프링 컨트롤러로 인식. 스프링 컨트롤러로 인식해야, HTTP URL이 매핑되고 동작
 @ResponseBody
 public interface OrderControllerV1 {
 
