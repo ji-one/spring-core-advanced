3919177ec03859567bd1153e8091992fecf845a2
ji-one <ds020228@naver.com>
2023-08-20 18:27:21 +0900

    feat: JDK 동적 프록시 - 소개

---
+++ src/test/java/hello/springcoreadvanced/jdkdynamic/code/AInterface.java
@@ -0,0 +1,5 @@
+package hello.springcoreadvanced.jdkdynamic.code;
+
+public interface AInterface {
+    String call();
+}
---
+++ src/test/java/hello/springcoreadvanced/jdkdynamic/code/BInterface.java
@@ -0,0 +1,5 @@
+package hello.springcoreadvanced.jdkdynamic.code;
+
+public interface BInterface {
+    String call();
+}
